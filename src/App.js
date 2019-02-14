985de254d20803a6222327dedcc0cc0aa184b494
Cijo Kizhakkoot Baby <cijo.kb@impetus.co.in>
2019-02-14 10:40:31 +0530

    Intial setup for Appolo Client

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { ApolloProvider, Query } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import gql from "graphql-tag";
+
+const httpLink = new HttpLink({
+  uri: "https://grpahiql.herokuapp.com/v1alpha1/graphql"
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+const getPosts = gql`
+query getFilms{
+  allFilms {
+    films{
+      title
+    }
+  }
+}
+`;
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <ApolloProvider client={client}>
+          <Query query={getPosts}>
+            {({ data, error, loading }) => {
+              if (loading) return <div>Loading...</div>;
+              if (error) return <div>Error...</div>;
+              return data.allFilms.films.map(function (film, index) {
+                return <li index>{film.title}</li>
+              });
+            }}
+          </Query>
+        </ApolloProvider>
       </div>
     );
   }
